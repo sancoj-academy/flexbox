5de6bf359d0f24bcb7a6c0991178e46e74f6c6b3
Ngozi Emeh <zingo1ng@yahoo.com>
2019-05-30 21:57:10 +0100

    long n lat

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function initMap() {
+    // Your location
+    const loc = {lat: 6.445261, lng: 3.418092}; 
+
+    // DOM manipulation
+    const map = new google.maps.Map(document.getElementById('map'), 
+    {
+        zoom: 12,
+        center: loc
+  });
+    
+    const marker = new google.maps.Marker({
+        position: loc, map: map
+    });
+}
