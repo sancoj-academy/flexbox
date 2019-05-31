e2a7c918620431310b69dd6ce326942d1481fc0a
Ngozi Emeh <zingo1ng@yahoo.com>
2019-05-31 21:52:51 +0100

    map script

--- main.js
+++ main.js
@@ -4,13 +4,12 @@ function initMap() {
     const loc = {lat: 6.445261, lng: 3.418092}; 
 
     // DOM manipulation
-    const map = new google.maps.Map(document.getElementById('map'), 
+    const map = new google.maps.Map(document.querySelector('.map'), 
     {
-        zoom: 12,
+        zoom: 8,
         center: loc
   });
     
-    const marker = new google.maps.Marker({
-        position: loc, map: map
+    const marker = new google.maps.Marker({ position: loc, map: map
     });
 }
